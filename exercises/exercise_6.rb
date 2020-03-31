4e5313e091ed4cec5680fae8b05e9babf157c203
Berk Ozer <galipberkozer@gmail.com>
2020-03-30 22:19:29 -0400

    Change employee hourly_rates to be in 40-200 range

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,12 +11,12 @@ puts "----------"
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 
-@store1.employees.create(first_name: "Yessino", last_name: "Nopanca", hourly_rate: 34)
+@store1.employees.create(first_name: "Yessino", last_name: "Nopanca", hourly_rate: 66)
 
 @store1.employees.create(first_name: "Sally", last_name: "Waxyon", hourly_rate: 53)
 
-@store2.employees.create(first_name: "Berk", last_name: "Ozer", hourly_rate: "1000")
+@store2.employees.create(first_name: "Berk", last_name: "Ozer", hourly_rate: 200)
 
-@store2.employees.create(first_name: "Jack", last_name: "Worrington", hourly_rate: 22)
+@store2.employees.create(first_name: "Jack", last_name: "Worrington", hourly_rate: 76)
 
 @store2.employees.create(first_name: "Opalo", last_name: "Konkon", hourly_rate: 44)
